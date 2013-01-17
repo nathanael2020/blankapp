e05c56c683e8d28410dc2d71a1638e66d7d47872
Nathanael Miksis <nathanael.miksis@gmail.com>
2013-01-17 11:46:00 -0800

    Changed db user to nathanael

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ logger.level = Capistrano::Logger::DEBUG
  
 # Since we're using pty, load the path ourselves
 set :default_environment, {
-  "PATH" => "/u/apps/blankapp:/home/nathanael/.rbenv/shims:/home/nathanael/.rbenv/bin:$PATH"
+  "PATH" => "/home/nathanael/.rbenv/shims:/home/nathanael/.rbenv/bin:$PATH"
 }
  
  
@@ -63,10 +63,19 @@ namespace :deploy do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       # Only precompile assets if any assets changed
       # http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
+      begin
+
       from = source.next_revision(current_revision)
-      if fetch(:force_assets, false) || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
+      
+      rescue 
+        err_no = true
+       end
+      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      
+#      if fetch(:force_assets, false) || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
         # Just like original: https://github.com/capistrano/capistrano/blob/master/lib/capistrano/recipes/deploy/assets.rb
-        run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
+#        run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
       end
